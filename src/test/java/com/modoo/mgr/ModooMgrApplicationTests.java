c979c944c9ac0b9dca68b82082a34f311d5d93bf
psk <psk3060@naver.com>
2023-03-31 01:00:07 +0900

    FIRST COMMIT

---
+++ src/test/java/com/modoo/mgr/ModooMgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modoo.mgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModooMgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
