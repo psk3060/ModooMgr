6fc781dd785cefebe4b38607d1603a6f484a3829
psk <psk3060@naver.com>
2023-03-31 01:51:20 +0900

    20230331[2] - Admin Test INIT

---
+++ src/test/java/com/modoo/mgr/AdminServiceTest.java
@@ -0,0 +1,29 @@
+package com.modoo.mgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AdminServiceTest {
+	
+	@Test
+	void adminSave() {
+		
+	}
+	
+	@Test
+	void adminFakeDelete() {
+	}
+	
+	@Test
+	void adminAuth() {
+		
+	}
+	
+	@Test
+	void adminLogin() {
+		
+	}
+	
+	
+}
